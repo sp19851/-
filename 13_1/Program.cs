831ad627692ad6a0a548cbab3bdfbe9827b47dc8
sp19851@gmail.com <sp19851@gmail.com>
2024-02-21 06:11:21 +0200

    Добавьте файлы проекта.

---
+++ 13_1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_1
+{
+    //базовый класс
+    class TwoDShape
+    {
+        public double width;
+        public double height;
+
+        public void ShowDim()
+        {
+            Console.WriteLine("Ширина и высота равны " + width + " и " + height);
+        }
+    }
+
+    //клас наследник
+    class Triangle : TwoDShape
+    {
+        public string style; //тип треугольника, переменная только в наследнике
+                             //метод расчитывает площадь треугольника
+        public double Area()
+        {
+            return width * height / 2;
+        }
+        //метод выводит информацию о типе треугольника
+        public void ShowStyle()
+        {
+            Console.WriteLine("Треугольник" + style);
+        }
+    }
+    class Shapes
+    {
+        static void Main()
+        {
+            Triangle triangle1 = new Triangle();
+            Triangle triangle2 = new Triangle();
+            triangle1.width = 4d;
+            triangle1.height = 4d;
+            triangle1.style = "равнобедренный";
+            triangle2.width = 8d;
+            triangle2.height = 12d;
+            triangle2.style = "прямоугольный";
+            Console.WriteLine("Сведения об объекте triangle1: ");
+            triangle1.ShowStyle();
+            triangle1.ShowDim();
+            Console.WriteLine("Площадь равна: "+ triangle1.Area());
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Сведения об объекте triangle2: ");
+            triangle2.ShowStyle();
+            triangle2.ShowDim();
+            Console.WriteLine("Площадь равна: " + triangle2.Area());
+
+            Console.ReadLine();
+
+        }
+    }
+}
